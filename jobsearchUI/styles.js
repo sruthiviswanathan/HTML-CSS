d529ee315b7de62a68680d039bc0f65dc51b96fd
sruthiviswanathan <ssravan1097@gmail.com>
2019-02-01 22:50:59 +0530

    added

---
+++ jobsearchUI/styles.js
@@ -0,0 +1,9 @@
+function openNav() {
+    // document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    // document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidenav").style.width = "0";
+}
