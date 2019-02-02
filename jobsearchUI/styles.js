a5d7aba7d4efcf96c44d5c8c66c52cd1e84705e5
sruthiviswanathan <ssravan1097@gmail.com>
2019-02-03 01:33:31 +0530

    added all files

--- jobsearchUI/styles.js
+++ jobsearchUI/styles.js
@@ -7,3 +7,27 @@ function closeNav() {
     // document.getElementById("main").style.marginLeft = "0";
     document.getElementById("mySidenav").style.width = "0";
 }
+
+  function viewlogin() {
+    var x = document.getElementById("signup");
+    var y = document.getElementById("login");
+    x.style.display = "none";
+    y.style.display = "block";
+    y.classList.add('button');
+  }
+  
+  function viewsignup() {
+    var y = document.getElementById("signup");
+    var x = document.getElementById("login");
+    y.style.display = "block";
+    x.style.display = "none";
+
+  }
+  
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
